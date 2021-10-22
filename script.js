b0ea65863c2a152b846ef3e4f6358880d5e0266c
David Opoku <71300793+dopoku12@users.noreply.github.com>
2021-10-22 14:18:54 -0400

    updates

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 "strict"
+
+const resume=document.querySelector(".resume")
+const projects=document.querySelector(".projects")
+const aboutMe=document.querySelector(".aboutMe")
+const button=document.querySelector(".left-arrow")
+
+
+
+button.addEventListener("click",function(){
+
+        resume.classList.add(".hide")
+        projects.classList.add(".hide")
+        aboutMe.classList.add(".hide")
+        
+        console.log("working")
+
+})
